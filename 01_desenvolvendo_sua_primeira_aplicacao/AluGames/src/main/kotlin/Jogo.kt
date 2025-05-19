5a92e195acb467ae5410cd0b22ee8091c3f82121
Flávio Vicentini <fvpoapl@gmail.com>
2025-05-19 17:16:17 -0300

    Aula 2.01 - Kotlin: desenvolvendo sua primeira aplicação

---
+++ 01_desenvolvendo_sua_primeira_aplicacao/AluGames/src/main/kotlin/Jogo.kt
@@ -0,0 +1,5 @@
+class Jogo {
+    var nome = ""
+    var capa = ""
+    val descricao = ""
+}
--- 01_desenvolvendo_sua_primeira_aplicacao/AluGames/src/main/kotlin/Main.kt
+++ 01_desenvolvendo_sua_primeira_aplicacao/AluGames/src/main/kotlin/Main.kt
@@ -6,10 +6,14 @@ import java.net.http.HttpResponse
 fun main() {
     val url = "https://www.cheapshark.com/api/1.0/games?title=batman"
 
-    val client: HttpClient = HttpClient.newHttpClient()
+    val client = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder().uri(URI.create(url)).build()
     val response = client.send<String>(request, HttpResponse.BodyHandlers.ofString())
 
     println(response)
     println(response.body())
+
+    val jogo1 = Jogo()
+    jogo1.nome = "LEGO Batman"
+    jogo1.capa = "https:\\/\\/cdn.fanatical.com\\/production\\/product\\/400x225\\/105f34ca-7757-47ad-953e-7df7f016741e.jpeg"
 }
